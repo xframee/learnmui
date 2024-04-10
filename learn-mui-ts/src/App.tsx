d9dbe0e23a300b5d6437c9378217e627fc8d9fe5
xframee <alexander@kmax.dk>
2024-04-10 18:20:46 +0200

    Update App.tsx

--- learn-mui-ts/src/App.tsx
+++ learn-mui-ts/src/App.tsx
@@ -1,14 +1,24 @@
 import './App.css';
 //import { MuiTypography } from './componenets/MuiTypography';
 //import { MuiButton } from './componenets/MuiButton';
-import { MuiTextField } from './componenets/MuiTextField';
+//import { MuiTextField } from './componenets/MuiTextField';
+//import { MuiSelect } from './componenets/MuiSelect';
+//import { MuiRadio } from './componenets/MuiRadio';
+//import { MuiCheckbox } from './componenets/MuiCheckbox';
+//import { MuiSwitch } from './componenets/MuiSwitch';
+import { MuiRating } from './componenets/MuiRating';
 
 function App() {
   return (
     <div className='App'>
       {/*<MuiTypography />*/}
       {/*<MuiButton />*/}
-      {<MuiTextField />}
+      {/*<MuiTextField />*/}
+      {/*<MuiSelect />*/}
+      {/*<MuiRadio />*/}
+      {/*<MuiCheckbox />*/}
+      {/*<MuiSwitch />*/}
+      <MuiRating />
     </div>
   );
 }
