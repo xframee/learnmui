c19466cde0b0bf1314100ca62b73c2855b480c3b
xframee <alexander@kmax.dk>
2024-04-10 18:21:30 +0200

    Update MuiTextField.tsx

--- learn-mui-ts/src/componenets/MuiTextField.tsx
+++ learn-mui-ts/src/componenets/MuiTextField.tsx
@@ -1,13 +1,37 @@
-import { TextField, Stack } from "@mui/material";
+import { TextField, Stack, InputAdornment } from "@mui/material";
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
 export const MuiTextField = () => {
     return (
         <div>
             <Stack spacing={4}>
                 <Stack direction="row" spacing={2}>
-                    <TextField label='Name' variant="outlined"/>
-                    <TextField label='Name' variant="filled"/>
-                    <TextField label='Name' variant="standard"/>
+                    <TextField label='Name' variant="outlined" />
+                    <TextField label='Name' variant="filled" />
+                    <TextField label='Name' variant="standard" />
+                </Stack>
+                <Stack direction="row" spacing={2}>
+                    <TextField label='Small secondary' size="small" color="secondary" />
+                </Stack>
+                <Stack direction="row" spacing={2}>
+                    <TextField label='Form input' required />
+                    <TextField
+                        label='Password'
+                        helperText='Do not share your password with anyone'
+                        type='password'
+                    />
+                    <TextField
+                        label='Password'
+                        helperText='Do not share your password with anyone'
+                        type='password'
+                        InputProps={{
+                            endAdornment: (
+                                <InputAdornment position='end'>
+                                    <VisibilityOffIcon />
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
                 </Stack>
             </Stack>
         </div>
